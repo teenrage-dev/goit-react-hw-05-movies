86c702a398c830a12692b3c9ac3fa3b35cedd4ae
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-08-22 21:40:35 +0300

    Summary Done all

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from './components/Home/Home';
+import { Layout } from './components/Layout/Layout';
+import { MoviesSearch } from './components/MoviesSearch/MoviesSearch';
+import { MoviesDetails } from './components/MoviesDetails/MoviesDetails';
+import { MoviesCast } from './components/MoviesCast/MoviesCast';
+import { MoviesReviews } from './components/MoviesReviews/MoviesReviews';
+import { MoviesLayout } from './components/MoviesLayout/MoviesLayout';
+
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="movies" element={<MoviesLayout />}>
+            <Route index element={<MoviesSearch />} />
+            <Route path=":moviesId" element={<MoviesDetails />}>
+              <Route path="cast" element={<MoviesCast />} />
+              <Route path="reviews" element={<MoviesReviews />} />
+              <Route
+                path="*"
+                element={
+                  <div>
+                    <h2>Not Found</h2>
+                  </div>
+                }
+              />
+            </Route>
+
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h2>Not Found</h2>
+                </div>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <div>
+                <h2>Not Found</h2>
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+//
+// / - Home
+// /movies - Movies
+//  - /movies/:movieId - Movie Details
+//    - /movies/:movieId/cast - Cast
+//    - /movies/:movieId/reviews - Reviews
